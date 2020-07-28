13967f1fee467eb5b580478af974c0bbfdc4b664
CoderJoe82 <coderjoe82@gmail.com>
2020-07-28 18:22:15 -0400

    initial commit

--- iss.py
+++ iss.py
@@ -3,9 +3,22 @@
 __author__ = '???'
 
 
-def main():
-    pass
+from pip._vendor import requests
 
+
+def main():
+    r = requests.get('http://api.open-notify.org/astros.json').json()
+    # print(r['people'])
+    people = []
+    for person in r['people']:
+        people.append(person['name'])
+    print(f"Names: {people}")
+
+    space = requests.get('http://api.open-notify.org/iss-now.json').json()
+    print(f"Geographical location; Lat: {space['iss_position']['latitude']}, Lon: {space['iss_position']['longitude']}")
+    print(f"Timestamp: {space['timestamp']}")
+
+    
 
 if __name__ == '__main__':
     main()
